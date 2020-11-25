e8e1fcc19e8391cd83109c27f3175f087cde781a
tony <1539670722@qq.com>
2020-11-25 21:20:21 +0800

    暂时让两个登记页面登记后返回index页面

--- Controllers/DutyController.cs
+++ Controllers/DutyController.cs
@@ -25,22 +25,23 @@ namespace DormitoryManagement.Controllers
             return View();
         }
 
-        public void sign_in_stranger(string p_id,string name,string in_time,string out_time,string type)
+        public IActionResult sign_in_stranger(string p_id,string name,string in_time,string out_time,string type)
         {
             if (p_id != null)
             {
                 Sql.Execute("INSERT INTO tableofperson VALUES (NULL,@0,@1,@2,@3,@4)",p_id, name, in_time, out_time, type);
                 //Sql.Execute("INSERT INTO tableofperson(p_id,name,in-time,out-time,type) VALUES (@0,@1,@2,@3,@4)", p_id, name, in_time, out_time, type);
             }
-
+            return View("Index");
         }
 
-        public void sign_in_late_student(string p_id,string name,string in_time,string type)
+        public IActionResult sign_in_late_student(string p_id,string name,string in_time,string type)
         {
             if (p_id!= null)
             {
                 Sql.Execute("INSERT INTO tableofperson VALUES(NULL,@0,@1,@2,NULL,@3)", p_id, name, in_time, type);
             }
+            return View("Index");
         }
     }
 }
