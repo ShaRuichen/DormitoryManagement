04900dc7fd7d0733f8fde82af52360c787c7445b
ShaRuichen <1747308644@qq.com>
2020-12-22 15:46:14 +0800

    s

--- wwwroot/js/manager.js
+++ wwwroot/js/manager.js
@@ -1,1 +1,19 @@
-﻿
+﻿function manageentrance() {
+    window.location.href = '/manager/manageentrance'
+}
+
+function searchemptybed() {
+    window.location.href = '/manager/searchemptybed'
+}
+
+function changetable() {
+    window.location.href = '/manager/changetable'
+}
+
+function showstudenttable() {
+    window.location.href = '/manager/showstudenttable'
+}
+
+function showunpaymenttable() {
+    window.location.href = '/manager/showunpaymenttable'
+}
