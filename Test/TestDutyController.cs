4e344a808f6453f2d47919708445f0a991895a9c
ShaRuichen <1747308644@qq.com>
2020-12-30 14:00:12 +0800

    新版

---
+++ Test/TestDutyController.cs
@@ -0,0 +1,91 @@
+﻿using DormitoryManagement.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Test
+{
+    [TestClass]
+    class TestDutyController
+    {
+        private DutyController controller;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            controller = new DutyController();
+        }
+
+        [TestMethod]
+        public void Test_sign_in_stranger_success()
+        {
+            var query = DormitoryManagement.Sql.Read("SELECT * FROM tableofperson");
+            int preNum = query.Count;                   //调用sign_in_stranger函数前的列数
+
+            string p_id = "12345", name = "name1", in_time = "2020/12/20-15:00", out_time = "2020/12/20-16:30", type = "stranger";
+
+            controller.sign_in_stranger(p_id,name,in_time,out_time,type);
+           
+            int postNum = query.Count;                  //调用sign_in_stranger函数后的列数
+            var result = (preNum == postNum-1);         //preNum与postNum的比较，如果两个值一致返回true
+
+            Assert.IsTrue(result);
+        }
+
+
+        [TestMethod]
+        public void Test_sign_in_stranger_failed()
+        {
+            var query = DormitoryManagement.Sql.Read("SELECT * FROM tableofperson");
+            int preNum = query.Count;                   //调用sign_in_stranger函数前的列数
+
+            string p_id = null, name = "name1", in_time = "2020/12/20-15:00", out_time = "2020/12/20-16:30", type = "stranger";
+
+            controller.sign_in_stranger(p_id, name, in_time, out_time, type);
+
+            int postNum = query.Count;                  //调用sign_in_stranger函数后的列数
+            var result = (preNum != postNum - 1);       //preNum与postNum的比较，如果两个值不一致返回true
+
+            Assert.IsTrue(result);
+        }
+        
+       //虽然数据库里p_id定义int型，但是在这里它用string型。因为修改int型了就不能用sign_in_stranger里的if(p_id!=null)这条件
+         
+         
+
+
+
+        [TestMethod]
+        public void Test_sign_in_late_student_success()
+        {
+            var query = DormitoryManagement.Sql.Read("SELECT * FROM tableofperson");
+            int preNum = query.Count;                   //调用sign_in_late_student函数前的列数
+
+            string p_id = "student1", name = "name1", in_time = "2020/12/21-23:30", type = "late student";
+            controller.sign_in_late_student(p_id, name, in_time, type);
+
+            int postNum = query.Count;                  //调用sign_in_late_student函数后的列数
+            var result = (preNum == postNum - 1);       //preNum与postNum的比较，如果两个值一致返回true
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Test_sign_in_late_student_failed()
+        {
+            var query = DormitoryManagement.Sql.Read("SELECT * FROM tableofperson");
+            int preNum = query.Count;                   //调用sign_in_late_student函数前的列数
+
+            string p_id = null, name = "name1", in_time = "2020/12/21-23:30", type = "late student";
+            controller.sign_in_late_student(p_id, name, in_time, type);
+
+            int postNum = query.Count;                  //调用sign_in_late_student函数后的列数
+            var result = (preNum != postNum - 1);       //preNum与postNum的比较，如果两个值不一致返回true
+
+            Assert.IsTrue(result);
+        }
+    }
+}
+
+
---
+++ Test/TestManagerController.cs
@@ -0,0 +1,60 @@
+﻿using DormitoryManagement.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    class TestManagerController
+    {
+        private ManagerController controller;
+
+       [TestInitialize] 
+        public void Initialize()
+        {
+            controller = new ManagerController();
+        }
+
+        //管理门禁权限_是
+        [TestMethod]
+        public void Test_ManageEntrance_yes()
+        {
+            string s_id = "12345";
+            string s_open = "yes";
+            controller.EntranceTable(s_id, s_open);
+
+            var actual = DormitoryManagement.Sql.Read("SELECT id,authority FROM student WHERE id = @0,authrity = @1", s_id,s_open);
+            var expect = DormitoryManagement.Sql.Read("SELECT id,authority FROM student WHERE id = @0",s_id);     
+
+            Assert.AreEqual(actual, expect);
+        }
+        
+        //管理门禁权限_否
+        [TestMethod]
+        public void Test_ManageEntrance_no()
+        {
+            string s_id = "12345";
+            string s_open = "No";
+            controller.EntranceTable(s_id, s_open);
+
+            var actual = DormitoryManagement.Sql.Read("SELECT id,authority FROM student WHERE id = @0,authrity = @1", s_id, s_open);
+            var expect = DormitoryManagement.Sql.Read("SELECT id,authority FROM student WHERE id = @0,", s_id);
+
+            Assert.AreEqual(actual, expect);
+        }
+
+
+        [TestMethod]
+        public void Test_ChangeStudent()
+        {
+            string s_id = "12345";
+            string s_bed = "宿舍1 201";
+            var s_bedParts = s_bed.Split(' ');
+
+            controller.ChangeStudent(s_id, s_bed);
+
+            var quary = DormitoryManagement.Sql.Read("SELECT * FROM student WHERE id = @0,building=@1,room=@2", s_id, s_bedParts[0], s_bedParts[1]);
+            Assert.IsNotNull(quary);
+        }
+    }
+}
